7e93491fdd21da362486d52e2c4864230b9af7b2
Tiep Vu <vuhuutiep@gmail.com>
2020-07-01 16:47:47 -0700

    base test base

---
+++ tests/base/test_utils.py
@@ -0,0 +1,18 @@
+from qcore.asserts import assert_eq
+import random
+
+from kfc.base.utils import random_string
+
+
+class TestRandomString:
+    def test_length(self):
+        length = random.randint(1, 100)
+        str1 = random_string(length)
+        # sanity check
+        assert_eq(length, len(str1))
+
+    def test_different_each_time(self):
+        str1 = random_string(5)
+        str2 = random_string(5)
+        # check two calls return differnt strings
+        assert_eq(2, len(set([str1, str2])))
