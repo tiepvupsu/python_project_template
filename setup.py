48b22267ab8bdfeafaedcf35253457bfa868eb89
tiepvupsu <vuhuutiep@gmail.com>
2020-07-01 17:33:10 -0700

    Lint (#2)

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
-# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
-from setuptools import setup, find_packages
-
-
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name="kfc",
+    version="0.1.0",
+    description="Data sicence with tf feature columns",
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author="Tiep Vu",
+    author_email="vuhuutiep@gmail.com",
+    url="https://github.com/tiepvupsu/kfcd",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=("tests", "docs")),
 )
-
