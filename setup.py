9c42212b1f364bc2570cdcfe41a01157edffea71
Tiep Vu <vuhuutiep@gmail.com>
2021-10-08 22:24:38 -0700

    add setup.*

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name="python_project_template",
+    packages=setuptools.find_packages(),
+    version="0.0.1",
+    license="apache-2.0",
+    description="Your package description",
+    author="Your Name",
+    author_email="your@email.com",
+    url="https://github.com/tiepvupsu/tabml",
+    download_url="",
+    keywords=["template"],
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
